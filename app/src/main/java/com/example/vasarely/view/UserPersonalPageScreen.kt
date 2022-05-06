22a739ea2cd5ccaabc45c3493ec56d5cd14f562e
kyrylost <kereech2@gmail.com>
2022-05-06 21:01:23 +0300

    Fixes. Don't forget to pull, before start working

--- app/src/main/java/com/example/vasarely/view/UserPersonalPageScreen.kt
+++ app/src/main/java/com/example/vasarely/view/UserPersonalPageScreen.kt
@@ -1,11 +1,13 @@
 package com.example.vasarely.view
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -32,15 +34,29 @@ class UserPersonalPageScreen: Fragment(R.layout.user_personal_page_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val popupView = layoutInflater.inflate(R.layout.menu_user_personal_page_screen, null)
-        val btnCN = popupView.findViewById<TextView>(R.id.settings_nickname_text)
-        val popupView1 = layoutInflater.inflate(R.layout.nickname_change_popup, null)
-        val changeNick = popupView1.findViewById<TextInputEditText>(R.id.username_input)
-        val btnSave = popupView1.findViewById<Button>(R.id.button)
+
+        val nicknamePopup = layoutInflater.inflate(R.layout.nickname_change_popup, null)
+        val changeNicknameTextView = popupView.findViewById<TextView>(R.id.settings_nickname_text)
+        val btnSaveNickname = nicknamePopup.findViewById<Button>(R.id.button)
+        val changeNick = nicknamePopup.findViewById<TextInputEditText>(R.id.username_input)
+
+        val passwordPopup = layoutInflater.inflate(R.layout.change_password_popup, null)
+        val changePasswordTextView = popupView.findViewById<TextView>(R.id.key_text)
+
+        val logoutButton = popupView.findViewById<ImageButton>(R.id.logoutButton)
+        val logoutText = popupView.findViewById<TextView>(R.id.logout_text)
+
+        val likedPostButton = popupView.findViewById<ImageButton>(R.id.likedPostIcon)
+        val likedPostText = popupView.findViewById<TextView>(R.id.likedPostText)
+
+        val newPrefButton = popupView.findViewById<ImageButton>(R.id.newPrefButton)
+        val newPrefText = popupView.findViewById<TextView>(R.id.new_pref_text)
+
+
         //----------------------------Navigation between screens------------------------------------
         //to SearchScreen
         binding.searchButton.setOnClickListener {
-            val action =
-                UserPersonalPageScreenDirections.actionUserPersonalPageScreenToSearchScreen()
+            val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToSearchScreen()
             findNavController().navigate(action)
         }
         //to MainScreen
@@ -50,8 +66,7 @@ class UserPersonalPageScreen: Fragment(R.layout.user_personal_page_screen) {
         }
 
         binding.menuImgBtn.setOnClickListener {
-            val action =
-                UserPersonalPageScreenDirections.actionUserPersonalPageScreenToNewPreferencesScreen()
+            val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToNewPreferencesScreen()
             findNavController().navigate(action)
         }
 
@@ -70,35 +85,82 @@ class UserPersonalPageScreen: Fragment(R.layout.user_personal_page_screen) {
             val dialogBuilder = AlertDialog.Builder(context)
             dialogBuilder.setView(popupView)
 
-            val addNoteDialog = dialogBuilder.create()
-            addNoteDialog.show()
+            val settingsDialog = dialogBuilder.create()
+            settingsDialog.show()
 
+            logoutButton.setOnClickListener {
+                appViewModel.logout()
+
+                val sharedPref = activity?.getSharedPreferences("userLoginData", Context.MODE_PRIVATE) ?: return@setOnClickListener
+                val editor = sharedPref.edit()
+                editor.putString("remember", "false")
+                editor.apply()
+
+                settingsDialog.dismiss()
+
+                val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToSignInSignUpScreen()
+                findNavController().navigate(action)
             }
-        btnCN.setOnClickListener {
-            val dialogBuilder1 = AlertDialog.Builder(context)
 
-            dialogBuilder1.setView(popupView1)
-            val addNoteDialog1 = dialogBuilder1.create()
-            addNoteDialog1.show()
-            
-        }
-        btnCN.setOnClickListener {
-                val dialogBuilder1 = AlertDialog.Builder(context)
+            likedPostButton.setOnClickListener {
+                val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToLikedPostScreen()
+                findNavController().navigate(action)
 
-                dialogBuilder1.setView(popupView1)
-                val addNoteDialog1 = dialogBuilder1.create()
-                addNoteDialog1.show()
-            btnSave.setOnClickListener {
-                val newnickname = changeNick.text.toString()
-                if (newnickname.isNotEmpty()) {
-                    appViewModel.updateName(newnickname)
-                    addNoteDialog1.dismiss()
-                } else if (newnickname.isEmpty()) {
-                    changeNick.error = getString(R.string.empty_username_error_message)
+                settingsDialog.dismiss()
+            }
+
+            likedPostText.setOnClickListener {
+                val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToLikedPostScreen()
+                findNavController().navigate(action)
+
+                settingsDialog.dismiss()
+            }
+
+            newPrefButton.setOnClickListener {
+                val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToNewPreferencesScreen()
+                findNavController().navigate(action)
+
+                settingsDialog.dismiss()
+            }
+
+            newPrefText.setOnClickListener {
+                val action = UserPersonalPageScreenDirections.actionUserPersonalPageScreenToNewPreferencesScreen()
+                findNavController().navigate(action)
+
+                settingsDialog.dismiss()
+            }
+
+            changePasswordTextView.setOnClickListener {
+                val dialogBuilderPassword = AlertDialog.Builder(context)
+                dialogBuilderPassword.setView(passwordPopup)
+
+                val changePasswordDialog = dialogBuilderPassword.create()
+                changePasswordDialog.show()
+            }
+
+            changeNicknameTextView.setOnClickListener {
+                val dialogBuilderNickname = AlertDialog.Builder(context)
+                dialogBuilderNickname.setView(nicknamePopup)
+
+                val changeNicknameDialog = dialogBuilderNickname.create()
+                changeNicknameDialog.show()
+
+                btnSaveNickname.setOnClickListener {
+                    val newNickname = changeNick.text.toString()
+                    if (newNickname.isNotEmpty()) {
+                        appViewModel.updateName(newNickname)
+                        changeNicknameDialog.dismiss()
+                    } else if (newNickname.isEmpty()) {
+                        changeNick.error = getString(R.string.empty_username_error_message)
+                    }
                 }
             }
+
         }
-        }
+
+
+
+    }
 
 
 
